362cb3aabe16d56dc2c4e74e8b761b9e4216fc9a
Ihor.Kucheriavyi <ihor.kucheriavyi@avenga.com>
2022-11-13 12:23:45 +0200

    Initial commit

---
+++ src/main/java/com/example/ihorko/sapr/SaprApplication.java
@@ -0,0 +1,13 @@
+package com.example.ihorko.sapr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaprApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaprApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/ihorko/sapr/controller/BasePageController.java
@@ -0,0 +1,21 @@
+package com.example.ihorko.sapr.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BasePageController {
+
+    @GetMapping("/arrays")
+    public String arrayController(Model model) {
+        String[] continents = {
+                "Africa", "Antarctica", "Asia", "Australia",
+                "Europe", "North America", "Sourth America"
+        };
+
+        model.addAttribute("continents", continents);
+
+        return "continents";
+    }
+}
